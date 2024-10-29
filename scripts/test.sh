04a4b491309701562929e6c2c5a4dca79ccac6e7
Carlo Acosta <acostacarlom@gmail.com>
2024-10-29 17:16:27 +0800

    Add datasets for 1 and 3 samples

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 export PYTHONPATH="$PWD"
 
+# ------------------ Default test data ----------------------
 # python vh/data_gene/gen_data/vh_init.py --display 0 --port "8083" --task all --mode simple --usage train --num-per-apartment 500 
 # python vh/data_gene/testing_agents/gene_data.py --mode simple \
 #    --dataset_path ./vh/dataset/env_task_set_500_simple.pik\
@@ -10,17 +11,25 @@ export PYTHONPATH="$PWD"
 #    --dataset_path ./vh/dataset/env_task_set_500_full.pik\
 #    --base-port 8104 
 
-python mcts/virtualhome/mcts_agent.py \
-    --exploration_constant 24 \
-    --max_episode_len 50 \
-    --max_depth 20 \
-    --round 0 \
-    --simulation_per_act 2 \
-    --simulation_num 100 \
-    --discount_factor 0.95  \
-    --uct_type PUCT \
-    --mode simple \
-    --seen_item \
-    --seen_apartment\
-    --model gpt-3.5-turbo-0125 \
-    --seen_comp
+
+# ------------------ Smaller test data ----------------------
+python vh/data_gene/gen_data/vh_init.py --display 0 --port "8083" --task all --mode simple --usage train --num-per-apartment 1
+python vh/data_gene/gen_data/vh_init.py --display 0 --port "8083" --task all --mode full --usage train --num-per-apartment 1
+python vh/data_gene/gen_data/vh_init.py --display 0 --port "8083" --task all --mode simple --usage train --num-per-apartment 3
+python vh/data_gene/gen_data/vh_init.py --display 0 --port "8083" --task all --mode full --usage train --num-per-apartment 3
+
+# ------------------ Run model ------------------------------
+# python mcts/virtualhome/mcts_agent.py \
+#     --exploration_constant 24 \
+#     --max_episode_len 50 \
+#     --max_depth 20 \
+#     --round 0 \
+#     --simulation_per_act 2 \
+#     --simulation_num 100 \
+#     --discount_factor 0.95  \
+#     --uct_type PUCT \
+#     --mode simple \
+#     --seen_item \
+#     --seen_apartment\
+#     --model gpt-3.5-turbo-0125 \
+#     --seen_comp
